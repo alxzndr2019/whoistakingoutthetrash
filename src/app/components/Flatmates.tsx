0df2423366b5acc04b39ebc35ab68a0637abca4f
Ohioze Alex Omiunu <ohiozeomiunu@gmail.com>
2024-09-25 06:50:04 +0100

    chore: error clean up

--- src/app/components/Flatmates.tsx
+++ src/app/components/Flatmates.tsx
@@ -1,6 +1,5 @@
 "use client";
 import React, { useState, useMemo } from "react";
-import { Calendar } from "@/components/ui/calendar";
 import { Card, CardHeader, CardTitle, CardContent } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
